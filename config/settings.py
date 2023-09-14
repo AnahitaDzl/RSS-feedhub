2412d241beb03d4835b4e6ef8e3931c92fa54d9d
AnahitaDzl <anahita.dozali@gmail.com>
2023-09-14 22:51:31 +0330

    chor: database changend to sql

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'RSS-feed',
+        'USER': 'postgres',
+        'PASSWORD': '9535',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
