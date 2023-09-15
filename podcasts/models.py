2cb22ef29bd818886e90b5984e5d2d4331d440b3
AnahitaDzl <anahita.dozali@gmail.com>
2023-09-15 08:12:32 +0330

    feat: XML model added

--- podcasts/models.py
+++ podcasts/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class XML(models.Model):
 
+    link = models.URLField(max_length=255, unique=True)
+    name = models.CharField(max_length=50, unique=True, default="Untitled")
+
+    def __str__(self):
+        return self.name
 
