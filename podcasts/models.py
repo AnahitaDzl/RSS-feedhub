08bfcf21450fbd128e54ea6c5a260cfb5d49fba2
AnahitaDzl <anahita.dozali@gmail.com>
2023-09-15 08:39:12 +0330

    feat: podcast model added

--- podcasts/models.py
+++ podcasts/models.py
@@ -1,11 +1,28 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
 class XML(models.Model):
-
     link = models.URLField(max_length=255, unique=True)
     name = models.CharField(max_length=50, unique=True, default="Untitled")
 
     def __str__(self):
         return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=70)
+
+class Podcast(models.Model):
+    title = models.CharField(max_length=100)
+    description=models.TextField(null=True, blank=True)
+    language = models.CharField(max_length=5, null=True, blank=True)
+    author = models.CharField(max_length=50, null=True)
+    image = models.URLField(max_length=255, null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.PROTECT, blank=True)
+    xml=models.ForeignKey(XML,on_delete=models.PROTECT,null=True, blank=True)
+    category=models.ForeignKey(Category,on_delete=models.PROTECT,null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
 
