79bcb4e8bab0b0a926a1bd2e5980bb0b9a02e38c
AnahitaDzl <anahita.dozali@gmail.com>
2023-09-15 08:45:26 +0330

    feat: subscription model added

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from podcasts.models import Podcast
 
 # Create your models here.
 class User(AbstractUser, PermissionsMixin):
@@ -9,3 +10,8 @@ class User(AbstractUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
+
+class Subscription(models.Model):
+        subscription_date = models.DateField()
+        user = models.ForeignKey(User, on_delete=models.PROTECT)
+        podcast = models.ForeignKey(User, on_delete=models.PROTECT)
