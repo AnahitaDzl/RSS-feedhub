f79cf3028aadb2968a6b22ea97d7318bed093920
AnahitaDzl <anahita.dozali@gmail.com>
2023-09-15 08:15:23 +0330

    feat: User model added

--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+
+# Create your models here.
+class User(AbstractUser, PermissionsMixin):
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(unique=True)
+    is_superuser = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
